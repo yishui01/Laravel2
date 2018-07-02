87f72764c4649ad28ce6038dc2c661c43e452128
zuiqiangqishao <727040917@qq.com>
2018-07-02 10:10:26 +0800

    完成登录注册以及验证码

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,21 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+/**************************************************************/
+//Auth::routes(); //等同于下面的这些路由，为了方便观看，替换为具体路由
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');

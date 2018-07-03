61178e3233e98cd06b8239a8a4041e053795f992
zuiqiangqishao <727040917@qq.com>
2018-07-03 21:38:57 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -24,9 +25,15 @@ class TopicObserver
     {
         $topic->body = clean($topic->body, 'user_topic_body'); //使用HTMLPurifier对内容自动进行过滤
         $topic->excerpt = make_excerpt($topic->body);
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
+    }
+    //添加/更新完之后触发
+    public function saved(Topic $topic)
+    {
+        // 使用翻译器对 title 进行翻译
+        //if ( ! $topic->slug) { //原本是slug字段无内容的时候再触发，更新时不会触发，现在去掉这个条件
+        //现在每次添加/更新的时候都会重新生成seo后缀
+            dispatch(new TranslateSlug($topic)); //放入队列中，而不是下面的同步执行
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        //}
     }
 }

19de2accfddc90360b49e8c7def416ce93be8783
zuiqiangqishao <727040917@qq.com>
2018-07-16 18:03:06 +0800

    加入缓存是否为redis的判断，不是就抛异常

--- public/index.php
+++ public/index.php
@@ -55,6 +55,8 @@ $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+if(env('CACHE_DRIVER') != 'redis')throw new \Exception('实在抱歉，请先配置好缓存驱动为redis');
+
 $response->send();
 
 $kernel->terminate($request, $response);

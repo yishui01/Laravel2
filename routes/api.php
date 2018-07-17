d7ecc7f2a24d6fb0a1e67d5531748bb23520da24
zuiqiangqishao <727040917@qq.com>
2018-07-17 21:22:47 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,18 @@ $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
 
-    // 发送短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 
 });
 

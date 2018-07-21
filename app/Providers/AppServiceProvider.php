dadb001360c1fc46d7388138f8a04eef81eb2b5c
zuiqiangqishao <727040917@qq.com>
2018-07-22 07:58:29 +0800

    notifications index

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,12 +15,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \App\Models\User::observe(\App\Observers\UserObserver::class);
-        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class); #添加了吗？
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
         Schema::defaultStringLength(191);
+
         \Carbon\Carbon::setLocale('zh');
+
+        if (env('APP_DEBUG') == 1) {
+            \DB::listen(function($query) {
+                $tmp = str_replace('?', '"'.'%s'.'"', $query->sql);
+                $tmp = vsprintf($tmp, $query->bindings);
+                $tmp = str_replace("\\","",$tmp);
+                \Log::info($tmp."\n\n\t");
+            });
+        }
+
     }
 
     /**

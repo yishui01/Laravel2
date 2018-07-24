55d1cd0483cf6618654260f79b57097e0cd35c71
zuiqiangqishao <727040917@qq.com>
2018-07-24 17:15:36 +0800

    weapp users store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -43,11 +43,13 @@ class UsersController extends Controller
 
     }
 
+    //返回用户信息
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
     }
 
+    //更新用户信息
     public function update(UserRequest $request)
     {
         $user = $this->user();
@@ -66,5 +68,57 @@ class UsersController extends Controller
     public function activedIndex(User $user)
     {
         return $this->response->collection($user->getActiveUsers(), new UserTransformer());
+    }
+
+    //小程序通过手机号注册接口（小程序不调用获取用户信息的接口，否则微信登录一个openid）
+    public function weappStore(UserRequest $request)
+    {
+        // 缓存中是否存在对应的 key
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        // 判断验证码是否相等，不相等反回 401 错误
+        if (!hash_equals((string)$verifyData['code'], $request->verification_code)) {
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 获取微信的 openid 和 session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+
+        // 如果 openid 对应的用户已存在，报错403
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户，请直接登录');
+        }
+
+        // 创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        // meta 中返回 Token 信息
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
     }
 }
